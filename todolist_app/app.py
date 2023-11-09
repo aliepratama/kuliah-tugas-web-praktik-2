87bf6d4f35a4bafc5647bd75e16719ff7d88dbff
aliepratama <aliepratama2@gmail.com>
2023-11-09 18:21:50 +0700

    feat: make all route and form

--- todolist_app/app.py
+++ todolist_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 
 
@@ -16,27 +16,58 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/list/')
+@app.route('/list/', methods=['GET'])
 def list():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM todo")
-    data = cur.fetchall()
-    print('DATA>>>>>>>>>>>>>', data)
-    cur.close()
-    return render_template('list.html', users=data)
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM todo ORDER BY kategori")
+        data = cur.fetchall()
+        print('DATA>>>>>>>>>>>>>', data)
+        cur.close()
+        return render_template('list.html', lists=data, count=len(data))
 
 
 @app.route('/form/', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
+        kategori_list = ('Penting', 'Sedang', 'Biasa')
         nama_kegiatan = request.form['nama_kegiatan']
         kategori = request.form['kategori']
         print(nama_kegiatan, kategori)
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO todo (nama_kegiatan, kategori) VALUES (%s, %s)', (nama_kegiatan, kategori))
+        cur.execute('INSERT INTO todo (nama_kegiatan, kategori) VALUES (%s, %s)', (nama_kegiatan, kategori_list.index(kategori)))
         mysql.connection.commit()
         cur.close()
+        return redirect('/list/')
     return render_template('form.html')
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM todo WHERE id=%s", (id, ))
+        data = cur.fetchall()
+        print('DATA>>>>>>>>>>>>>', data)
+        cur.close()
+        return render_template('form_edit.html', todo=data, id=id)
+    elif request.method == 'POST':
+        kategori_list = ('Penting', 'Sedang', 'Biasa')
+        nama_kegiatan = request.form['nama_kegiatan']
+        kategori = request.form['kategori']
+        print(nama_kegiatan, kategori)
+        cur = mysql.connection.cursor()
+        cur.execute('UPDATE todo SET nama_kegiatan=%s, kategori=%s WHERE id=%s', (nama_kegiatan, kategori_list.index(kategori), id))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/list/')
+
+@app.route('/delete/<id>')
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM todo WHERE id=%s', (id,))
+    mysql.connection.commit()
+    cur.close()
+    return redirect('/list/')
 
 if __name__ == '__main__':
     app.run(debug=True)
